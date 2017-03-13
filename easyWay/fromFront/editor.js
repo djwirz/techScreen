915b5129c0096e95a20769aa0ffeb091f26a0374
djwirz <djwirz@gmail.com>
2017-03-13 12:45:02 +0200

    added comments to editor.js

--- easyWay/fromFront/editor.js
+++ easyWay/fromFront/editor.js
@@ -1,3 +1,6 @@
+//A function to take the editor input and evaluate it
+//Called on a button submit
+//eval === evil, but this isn't production
 const runEditor = () => {
   let editorInput = editor.getValue()
   let evalInput = eval(editorInput)
