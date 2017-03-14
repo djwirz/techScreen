616dceb50d7f36b08870b453bb7c2f7e3f19644c
djwirz <djwirz@gmail.com>
2017-03-14 10:18:25 +0200

    added comments to answer.directive.js

--- client/app/components/answer/answer.directive.js
+++ client/app/components/answer/answer.directive.js
@@ -14,3 +14,10 @@ export const answerDirective = () => {
     replace: true
   }
 }
+
+//getting the template and controller
+//controller as vm stands for vm from https://johnpapa.net/angularjss-controller-as-and-the-vm-variable/
+//want access to pulls
+//bind to controller does exactly that, binds to the controller rather thatn $scope
+//E means element name only 
+//replace being true makes for less of a nested name look on the dom
